8c3a2390f909ed336aefa6b4256e4430c26751be
Ik <113038466+Ik1497@users.noreply.github.com>
2023-03-23 18:48:28 +0100

    Build

--- build.cjs
+++ build.cjs
@@ -12,7 +12,7 @@ fs.readdir(`./src/`, function (err, files) {
     if (!file.endsWith(`.md`)) return
 
     const htmlFileName = file.slice(11).replace(`.md`, `.html`)
-
+    console.log(htmlFileName)
     fs.readFile(`src/${file}`, `utf8`, (err, data) => {
       if (err) return
 
@@ -28,14 +28,38 @@ fs.readdir(`./src/`, function (err, files) {
 
 function createPage(html) {
 
-return `
-<html>
+return `<!DOCTYPE html>
+<html lang="en">
   <head>
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Site Meta -->
+    <meta name="og:site_name" content="Streamer.bot Actions">
+    <meta property="og:image" content="https://ik1497.github.io/assets/images/favicon.png">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#B80086">
+    <meta name="author" content="Ik1497">
+    
+    <!-- Page Meta -->
+    <meta property="og:title" content="Mute Indicator">
+    <title>Mute Indicator</title>
+    
+    <meta property="og:description" content="Mute Indicator so you can see if and what sources are muted.">
+    <meta name="description" content="Mute Indicator so you can see if and what sources are muted.">
+    
+    <!-- Scripts -->
+    <script src="/assets/js/head.js"></script>
+    <script src="/assets/js/main.js" defer></script>
+    
+    <!-- Styles -->
+    <link rel="stylesheet" href="/assets/css/style.css">    
   </head>
-
   <body>
-    ${html}
+    <main>
+      ${html}
+    </main>
   </body>
 </html>
 `
