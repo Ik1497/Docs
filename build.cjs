a1f05e8d4a1735446203f8ac80f4e3179563b374
Ik <113038466+Ik1497@users.noreply.github.com>
2023-03-23 17:54:44 +0100

    Edit build script

--- build.cjs
+++ build.cjs
@@ -1,1 +1,30 @@
-console.log(`BUILDING`)
+const fs  = require(`fs`)
+const glob = require(`glob`)
+const showdown  = require(`showdown`)
+
+let converter = new showdown.Converter()
+converter.setOption(`tables`, true)
+
+fs.readdir(`./src/`, function (err, files) {
+  if (err) return
+
+  files.forEach(function (file) {
+    if (!file.endsWith(`.md`)) return
+    console.log(file)
+
+    const htmlFileName = file.slice(11).replace(`.md`, `.html`)
+
+    fs.readFile(`src/${file}`, `utf8`, (err, data) => {
+      if (err) return
+
+      const markdown = data.split(`\r\n`).slice(7).join(`\n`)
+      console.log(converter.makeHtml(markdown))
+
+      fs.writeFile(`dist/${htmlFileName}`, converter.makeHtml(markdown), (err) => {
+        if (err) return
+      });
+    });
+  });
+});
+
+// let html = converter.makeHtml(text)
