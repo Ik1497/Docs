fa18c38b175216661c144c4e5c3fe5f44888da5f
Ik <113038466+Ik1497@users.noreply.github.com>
2023-04-26 02:50:02 +0200

    Build

--- build.cjs
+++ build.cjs
@@ -28,8 +28,22 @@ const customClassExt = {
   }
 };
 
+const vuetifyExtension = {
+  type: `output`,
+  filter: function (text) {
+    return text
+      .replaceAll(`<hr>`, `<v-divider style="margin-block: 1rem;"></v-divider>`)
+      .replaceAll(`<hr/>`, `<v-divider style="margin-block: 1rem;"></v-divider>`)
+      .replaceAll(`<hr />`, `<v-divider style="margin-block: 1rem;"></v-divider>`)
+      .replaceAll(`<code>`, `<v-code style="display: inline;">`)
+      .replaceAll(`</code>`, `</v-code>`)
+      .replaceAll(`<kbd>`, `<v-kbd style="display: inline;">`)
+      .replaceAll(`</kbd>`, `</v-kbd>`)
+  }
+};
+
 let converter = new showdown.Converter({
-  extensions: [...bindings, customClassExt]
+  extensions: [...bindings, customClassExt, vuetifyExtension]
 })
 
 converter.setOption(`tables`, true)
@@ -45,17 +59,6 @@ converter.setOption(`simplifiedAutoLink`, true)
 converter.setOption(`strikethrough`, true)
 converter.setOption(`tasklists`, true)
 converter.setOption(`underline`, true)
-
-const languages = [
-  `nl`,
-  `de`,
-  `es`
-]
-
-createFileAndFolder(`docs/e/index.html`, createEditorPage({}, `index`))
-
-fs.copyFileSync(`src/main.js`, `docs/main.js`)
-fs.copyFileSync(`src/style.css`, `docs/style.css`)
 
 fs.readdir(`./docs-src/`, function (err, files) {
   if (err) return
@@ -74,14 +77,7 @@ fs.readdir(`./docs-src/`, function (err, files) {
 
       metadata.pageId = file.replaceAll(`.md`, ``)
 
-      createPage(html, metadata, `docs/${metadata?.directory}/${htmlFileName}.html`, `/Docs/${metadata?.directory}/${htmlFileName}`)
-      createPage(html, metadata, `docs/en/${metadata?.directory}/${htmlFileName}.html`, `/Docs/${metadata?.directory}/${htmlFileName}`)
-      
-      // languages.forEach(async function (language) {
-      //    createFileAndFolder(`docs/${language}/${htmlFileName}.html`, createPage(translate(encodeURI(html), language), metadata))
-      // });
-
-      createFileAndFolder(`docs/e/${htmlFileName}.html`, createEditorPage(metadata, htmlFileName))
+      createPage(html, metadata, `docs/${metadata?.directory}/${htmlFileName}/index.html`, `/Docs/${metadata?.directory}/${htmlFileName}/`)
     });
   });
 });
@@ -98,125 +94,106 @@ async function createPage(html, metadata, path, navPath) {
     navigationGroup.groupItems.forEach(navGroupItem => {
       navGroupItem = navGroupItem.groupItem
 
-      if (navGroupItem.channel != `public`) return
-
-      navGroupHtml += `
-      <li class="navigation-list-item"><a class="${navGroupItem.icon}${navPath === navGroupItem.href ? ` active` : ``}" href="${navGroupItem.href}">${navGroupItem.name}</a></li>
-      `
+      if (navGroupItem.channel != `public`) {
+        navGroupHtml += `
+        <v-list-item v-if="visibilityChannel === \`beta\`" prepend-icon="${navGroupItem.icon.replace(`mdi `, ``)}" title="${navGroupItem.name}" href="${navGroupItem.href}" :active="currentPath === \`${navGroupItem.href}\`" active-color="primary"></v-list-item>
+        `
+      } else {
+        navGroupHtml += `
+        <v-list-item prepend-icon="${navGroupItem.icon.replace(`mdi `, ``)}" title="${navGroupItem.name}" href="${navGroupItem.href}" :active="currentPath === \`${navGroupItem.href}\`" active-color="primary"></v-list-item>
+        `
+      }
     });
 
     if (navGroupHtml != ``) {
       navigationHtml += `
-      <div class="navigation-group">
-        <p tabindex="0">${navigationGroup.name}</p>
-        <ul class="navigation-list">
-          ${navGroupHtml}
-        </ul>
-      </div>
+      <v-list-subheader>${navigationGroup.name}</v-list-subheader>
+      ${navGroupHtml}
       `
     }
   });
 
   createFileAndFolder(path, `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      
-      <!-- Site Meta -->
-      <meta name="og:site_name" content="Streamer.bot Actions">
-      <meta property="og:image" content="https://ik1497.github.io/assets/images/favicon.png">
-      <link rel="icon" href="https://ik1497.github.io/assets/images/favicon.png">
-      <meta property="og:type" content="website">
-      <meta name="theme-color" content="#B80086">
-      <meta name="author" content="Ik1497">
-      
-      <!-- Page Meta -->
-      <meta property="og:title" content="${metadata?.title}">
-      <title>${metadata?.title}</title>
-      
-      <meta property="og:description" content="${metadata?.description}">
-      <meta name="description" content="${metadata?.description}">
+  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vuetify@3.1.14/dist/vuetify.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vuetify@3.1.14/dist/vuetify.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.2.96/css/materialdesignicons.min.css" integrity="sha512-LX0YV/MWBEn2dwXCYgQHrpa9HJkwB+S+bnBpifSOTO1No27TqNMKYoAn6ff2FBh03THAzAiiCwQ+aPX+/Qt/Ow==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
-      <!-- Scripts -->
-      <script src="./main.js" defer></script>
-      <script src="https://ik1497.github.io/components/main.js"></script>
-      
-      <!-- Styles -->
-      <link rel="stylesheet" href="./style.css">
-    </head>
-    <body data-page-id="${metadata?.pageId}">
-      ${createHeader()}
-      <div class="main-content">
-        <nav class="primary-navigation">
-          <div class="navigation-overlay"></div>
-          <div class="navigation-content">
+  <div id="app">
+    <v-app>
+      <v-layout style="overflow-y: visible; overflow-x: hidden; position: relative; height: 100vh;">
+        <v-app-bar color="primary" style="position: fixed; top: 0; height: fit-content;">
+          <v-app-bar-nav-icon @click="navVisible = !navVisible"></v-app-bar-nav-icon>
+    
+          <v-app-bar-title>Streamer.bot Actions Docs</v-app-bar-title>
+        </v-app-bar>
+  
+        <v-navigation-drawer temporary v-model="navVisible" style="z-index: 1000; position: fixed; bottom: 0;">
+          <v-list density="compact" nav>
             ${navigationHtml}
-          </div>
-          <button class="navigation-close-button">Close</button>
-        </nav>
-        ${createToc()}
-        <main>
-          <h1>${metadata?.title}</h1>
+          </v-list>
+        </v-navigation-drawer>
+    
+        <v-main style="margin-top: 1rem; max-width: 100rem; margin-inline: auto; padding-inline: 1rem;">
           ${html}
-        </main>
-      </div>
-    </body>
-  </html>
+        </v-main>
+      </v-layout>
+    </v-app>
+  </div>
+  
+  <script>
+    const { createApp } = Vue
+    const { createVuetify } = Vuetify
+  
+    const vuetify = createVuetify({
+      theme: {
+        defaultTheme: \`dark\`,
+        themes: {
+          dark: {
+            colors: {
+              primary: \`#1E88E5\`,
+              secondary: \`#42A5F5\`
+            }
+          }
+        }
+      }
+    })
+  
+    const app = createApp({
+      data() {
+        return {
+          navVisible: false,
+          currentPath: location.pathname,
+          visibilityChannel: localStorage.getItem(\`websiteSettings__visibilityChannel\`)
+        }
+      }
+    })
+  
+    app.use(vuetify).mount(\`#app\`)
+  </script>
+  
+  <style>
+  html {
+    overflow-y: auto;
+  }
+
+  body {
+    font-family: Inter;
+  }
+  
+  img.img-400 {
+    max-width: 100%;
+    width: 40rem;
+  }
+  
+  p:not([class]) {
+    padding-bottom: 1rem;
+  }
+  </style>  
   `)
-}
-
-function createHeader() {
-  return `
-  <header class="primary-header">
-    <div class="header-content">
-      <div class="main">
-        <button class="navigation-button mdi mdi-menu"></button>
-        <img src="https://ik1497.github.io/assets/images/favicon.png" alt="favicon">
-        <p>Streamer.bot Actions</p>
-      </div>
-    </div>
-  </header>
-  `
-}
-
-function createToc() {
-  return ``
-}
-
-function createEditorPage(metadata, fileName, type = `page`) {
-  return `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      
-      <!-- Site Meta -->
-      <meta name="og:site_name" content="Streamer.bot Actions">
-      <meta property="og:image" content="https://ik1497.github.io/assets/images/favicon.png">
-      <link rel="icon" href="https://ik1497.github.io/assets/images/favicon.png">
-      <meta property="og:type" content="website">
-      <meta name="theme-color" content="#B80086">
-      <meta name="author" content="Ik1497">
-      
-      <!-- Page Meta -->
-      <meta property="og:title" content="Edit ${type === `page` ? metadata?.title : `Streamer.bot Actions`}">
-      <title>Edit ${type === `page` ? metadata?.title : `Streamer.bot Actions`}</title>
-      
-      <meta property="og:description" content="${type === `page` ? metadata?.description : `Edit the contents of the Streamer.bot Actions website`}">
-      <meta name="description" content="${type === `page` ? metadata?.description : `Edit the contents of the Streamer.bot Actions website`}">
-    </head>
-    <body ${type === `page` ? `data-page-id="${metadata?.pageId}" ` : ``}style="margin: 0; overflow: hidden; width: 100vw; height: 100vh;">
-      ${type === `page` ? `<iframe src="https://ik1497.netlify.app/#/collections/docs/entries/${metadata?.pageId}" style="width: 100%; height: 100%; overflow: hidden;"></iframe>` : `<iframe src="https://ik1497.netlify.app/#/collections/docs" style="width: 100%; height: 100%; overflow: hidden;"></iframe>`}
-      <a style="position: fixed; left: 1rem; bottom: 1rem; color: black; background: none; outline: 0; border: 0; border-radius: 0; cursor: pointer;" href="../${fileName}">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M22 12a10 10 0 0 1-10 10A10 10 0 0 1 2 12A10 10 0 0 1 12 2a10 10 0 0 1 10 10m-6.6 4.6L10.8 12l4.6-4.6L14 6l-6 6l6 6l1.4-1.4Z"/></svg>
-      </a>
-    </body>
-  </html>
-  `
 }
 
 function createFileAndFolder(path, content) {
@@ -231,30 +208,7 @@ function createFileAndFolder(path, content) {
     if (dirIndex > 0) addingPath += `/${dir}`
 
     fs.mkdirSync(addingPath, { recursive: true })
-    
-    fs.copyFileSync(`src/main.js`, `${addingPath}/main.js`)
-    fs.copyFileSync(`src/style.css`, `${addingPath}/style.css`)
   });
 
   fs.writeFileSync(`${addingPath}/${fileName}`, content)
-}
-
-function translate(text, to) {
-  const url = generateRequestUrl(text, { to: to });
-
-  createFileAndFolder(`test/${to}`, url)
-
-  https.get(url, (resp) => {
-    let data = ``
-  
-    resp.on(`data`, (chunk) => {
-      data += chunk
-    });
-
-    resp.on(`end`, () => {
-      return normaliseResponse(JSON.parse(data))
-    });
-  }).on(`error`, (err) => {
-    return `Error:`, err.message
-  });
 }
