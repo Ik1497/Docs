690dbbcefa5dff327f8d6ec96165b4d1215f4cd3
Ik <113038466+Ik1497@users.noreply.github.com>
2023-05-04 21:15:08 +0200

    Build

--- build.cjs
+++ build.cjs
@@ -94,7 +94,9 @@ const vuetifyExtension = {
       // @click="copy(\`${importCode}\`)"
       // @click="download(\`${importCode}\`)"
 
-      let newText = `<v-sheet rounded="lg" style="padding: 1rem; margin-bottom: 1rem;"><div style="display: flex; justify-content: space-between; align-items: center;"><div style="font-family: Poppins; font-weight: 500;">Import Code</div><div style="display: flex; gap: .5rem;"><v-btn variant="tonal">Copy</v-btn><v-btn variant="tonal">Download</v-btn></div></div><br><div style="overflow-x: auto; background: #181818; padding: 1rem; border-radius: .5rem .5rem 0  0; color: #aac8e4;">${importCode}</div></v-sheet>`
+      // <div style="display: flex; justify-content: space-between; align-items: center;"><div style="font-family: Poppins; font-weight: 500;">Import Code</div><div style="display: flex; gap: .5rem;"><v-btn variant="tonal">Copy</v-btn><v-btn variant="tonal">Download</v-btn></div></div>
+
+      let newText = `<v-sheet rounded="lg" style="padding: 1rem; margin-bottom: 1rem;"><br><div style="overflow-x: auto; background: #181818; padding: 1rem; border-radius: .5rem .5rem 0 0; color: #aac8e4;">${importCode}</div></v-sheet>`
     
       return newText
     });
